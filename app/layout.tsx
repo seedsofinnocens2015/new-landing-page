b3767c556c39dabf162d549116aee32b519d04fb
amit04-07-1999 <ameykumar76@gmail.com>
2026-01-19 12:34:12 +0530

    fg

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,11 +15,6 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Seeds Of Innocens Fertility & IVF",
   description: "Seeds Of Innocens Fertility & IVF - Best IVF Clinic",
-  icons: {
-    icon: '/icon.webp',
-    shortcut: '/icon.webp',
-    apple: '/icon.webp',
-  },
 };
 
 export default function RootLayout({
